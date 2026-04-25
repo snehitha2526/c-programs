d2828e7e701537d5c0a55931ef20f3ee5a15e502
snehitha2526 <130126214+snehitha2526@users.noreply.github.com>
2026-04-25 12:29:00 +0530

    Add files via upload

---
+++ weeks.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int cigars;
+    printf("Enter the number of cigars:\n");
+    scanf("%d", &cigars);
+
+    int weekend;
+    printf("Enter 1 for weekend and 0 for weekdays:\n");
+    scanf("%d", &weekend);
+
+    if (weekend == 1) {
+        if (cigars >= 40) {
+            printf("true");
+        } else {
+            printf("false");
+        }
+    } else {
+        if (cigars >= 40 && cigars <= 60) {
+            printf("true");
+        } else {
+            printf("false");
+        }
+    }
+
+    return 0;
+}
